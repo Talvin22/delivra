daf8964b9a4a54dd7b15cfe2840b986ec8cb78d2
Talvin22 <Talvin22@users.noreply.github.com>
2026-02-10 12:48:09 +0100

    createTask in controller created

--- src/main/java/site/delivra/application/controller/DeliveryTaskController.java
+++ src/main/java/site/delivra/application/controller/DeliveryTaskController.java
@@ -3,12 +3,10 @@ package site.delivra.application.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.delivra.application.model.constants.ApiLogMassage;
 import site.delivra.application.model.dto.DeliveryTaskDTO;
+import site.delivra.application.model.request.task.NewDeliveryTaskRequest;
 import site.delivra.application.model.response.DelivraResponse;
 import site.delivra.application.service.DeliveryTaskService;
 import site.delivra.application.utils.ApiUtils;
@@ -28,5 +26,15 @@ public class DeliveryTaskController {
         DelivraResponse<DeliveryTaskDTO> byId = deliveryTaskService.getById(id);
 
         return ResponseEntity.ok(byId);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<DelivraResponse<DeliveryTaskDTO>> createTask(@RequestBody NewDeliveryTaskRequest deliveryTaskRequest) {
+
+        log.trace(ApiLogMassage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+
+        DelivraResponse<DeliveryTaskDTO> deliveryTask = deliveryTaskService.createDeliveryTask(deliveryTaskRequest);
+
+        return ResponseEntity.ok(deliveryTask);
     }
 }
